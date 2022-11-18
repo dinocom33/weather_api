284a95a4a0acfd51ce5b03393275100208d4e7c3
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2022-11-18 20:59:55 +0200

    Update weather_api.py

--- weather_api.py
+++ weather_api.py
@@ -1,15 +1,15 @@
 import requests
 
-API_KEY = "put_your_API-KEY_here"
+API_KEY = "put_your_API_KEY_here"
 BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
 
 city_name = input("Enter a city name: ")
 request_url = f"{BASE_URL}?appid={API_KEY}&q={city_name}&units=metric&lang=bg"
 get_response = requests.get(request_url)
 
-if get_response.status_code == 200:  # this code means that the request is successful and we have a valid answer
+if get_response.status_code == 200:  # this code means that the request is successful, and we have a valid answer
     received_data = get_response.json()
-#    print(received_data)
+    # print(received_data)
     weather = received_data["weather"][0]["description"]
     temperature = received_data["main"]["temp"]
     feels_like = received_data["main"]["feels_like"]
@@ -17,7 +17,9 @@ if get_response.status_code == 200:  # this code means that the request is succe
     # max_temp = data["main"]["temp_max"]
     humidity = received_data["main"]["humidity"]
     wind = received_data["wind"]["speed"]
+    city = received_data["name"]
 
+    print(f"City: {city}")
     print(f"Weather: {weather}")
     print(f"Temperature: {temperature} °C")  # you can use chr(176) to display the celsius symbol
     print(f"Feels like: {feels_like} °C")
@@ -26,4 +28,4 @@ if get_response.status_code == 200:  # this code means that the request is succe
     print(f"Humidity: {humidity} %")
     print(f"Wind speed: {wind} m/s")
 else:
-    print("An error occurred. Please check the city name.")
+    print("An error occurred. Please check the city name or API key validity.")
